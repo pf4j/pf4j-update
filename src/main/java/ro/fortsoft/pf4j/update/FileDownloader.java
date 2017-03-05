082daafb12d44fa3582dc4b623ec28145edbd0c3
Decebal Suiu <decebal.suiu@gmail.com>
2017-03-05 20:11:57 +0200

    Format copyright header

--- src/main/java/ro/fortsoft/pf4j/update/FileDownloader.java
+++ src/main/java/ro/fortsoft/pf4j/update/FileDownloader.java
@@ -1,14 +1,17 @@
 /*
  * Copyright 2014 Decebal Suiu
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in compliance with
- * the License. You may obtain a copy of the License in the LICENSE file, or at:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package ro.fortsoft.pf4j.update;
 
@@ -86,9 +89,7 @@ class FileDownloader {
         while ((length = is.read(buffer)) >= 0) {
             fos.write(buffer, 0, length);
         }
-        if (fos != null) {
-            fos.close();
-        }
+        fos.close();
         is.close();
 
         // rename tmp file to resource file
