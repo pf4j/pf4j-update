5a3b1198f4d0b5382bc94dc89426a08735e6874c
Decebal Suiu <decebal.suiu@gmail.com>
2017-03-07 06:02:30 +0200

    Fix typo

--- src/main/java/ro/fortsoft/pf4j/update/DigestUtils.java
+++ src/main/java/ro/fortsoft/pf4j/update/DigestUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Decebal Suiu
+ * Copyright 2014 Decebal Suiu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
